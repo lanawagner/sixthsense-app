f7022b21a52b368c391edc25001a42696f073160
lanawagner <l.rose3002@gmail.com>
2019-11-08 11:59:47 -0500

    added basic swift structure

---
+++ SixthSense/SixthSense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SixthSense
+//
+//  Created by Diana Wagner on 11/8/19.
+//  Copyright © 2019 Diana Wagner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
