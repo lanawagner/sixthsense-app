2b909f095c203e8473ba59219c153415044335ca
lanawagner <l.rose3002@gmail.com>
2019-11-18 23:12:31 -0500

    UI Framework

---
+++ SixthSense/SixthSense/UI/LayoutSettings.swift
@@ -0,0 +1,19 @@
+//
+//  LayoutSettings.swift
+//  SixthSense
+//
+//  Created by Diana Wagner on 11/8/19.
+//  Copyright © 2019 Diana Wagner. All rights reserved.
+//
+
+import UIKit
+
+class LayoutSettings {
+    
+    //C O L O R   S C H E M E
+    static var darkBlue : UIColor = UIColor(red: 17/255, green: 63/255, blue: 103/255, alpha: 1.0)
+    static var mediumBlue : UIColor = UIColor(red: 52/255, green: 105/255, blue: 154/255, alpha: 1.0)
+    static var lightBlue : UIColor = UIColor(red: 101/255, green: 198/255, blue: 196/255, alpha: 1.0)
+    static var pink : UIColor = UIColor(red: 242/255, green: 166/255, blue: 166/255, alpha: 1.0)
+    
+}
